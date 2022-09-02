35919f2ce2252e2f3938396d2012bdcbb3b63e0d
Davide Gallotti <d.gallotti@outlook.it>
2022-09-02 21:04:36 +0200

    Fix grammar in the comments

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,16 +5,16 @@ pub(crate) trait Cpu {
     // Size of Opcode: it can to change.
     type OpCodeType;
 
-    // It generally takes the next instruction to PC
+    // It generally takes the next instruction from PC
     fn fetch(&self) -> Self::OpCodeType;
 
     // It decodes the instruction to understand the 
     // OpCode and the variables
     fn decode(&self, op_code: Self::OpCodeType) -> Condition;
 
-    // It executes the opcode and update registers and memory
+    // It executes the opcode and updates registers and memory
     fn execute(&self);
 
-    // Abstraction of what happens for every instruction
+    // Abstraction of what happens for every instruction in the cpu
     fn step(&self);
 }
