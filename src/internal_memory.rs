0881eea925e79e4f7074c1f7ce91973b081e7f80
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-09-28 17:23:14 +0200

    Memory: Add `Internal Memory` struct
    Here I just preparad interal "ram" but we can change the struct name in future based on how we want the granularity of
    our struct.
    
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

---
+++ src/internal_memory.rs
@@ -0,0 +1,29 @@
+use crate::io_device::IoDevice;
+
+pub struct InternalMemory {
+    /// From 0x03000000 to 0x03007FFF (32kb).
+    internal_work_ram: [u8; 0x7FFF],
+}
+
+impl InternalMemory {
+    pub fn new() -> Self {
+        Self {
+            internal_work_ram: [0; 0x7FFF],
+        }
+    }
+}
+
+impl IoDevice for InternalMemory {
+    type Address = u32;
+    type Value = u8;
+
+    fn read_at(&self, address: Self::Address) -> Self::Value {
+        let a: usize = address.try_into().unwrap();
+        self.internal_work_ram[a]
+    }
+
+    fn write_at(&mut self, address: Self::Address, value: Self::Value) {
+        let a: usize = address.try_into().unwrap();
+        self.internal_work_ram[a] = value;
+    }
+}
