b43fd55341c41c44bb92ccb41017bd3998e51989
Alessio Cosenza <cosenzaalessio17@gmail.com>
2022-11-07 23:44:37 +0100

    Everywhere: From `Rc<RefCell<>>` to `Arc<Mutex<>>`
    Preparatory work to implement the `Play` button.

--- emu/src/gba.rs
+++ emu/src/gba.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, rc::Rc};
+use std::sync::{Arc, Mutex};
 
 use crate::{
     arm7tdmi::Arm7tdmi,
@@ -11,20 +11,20 @@ pub struct Gba {
     pub cpu: Arm7tdmi,
 
     pub cartridge_header: CartridgeHeader,
-    pub cartridge: Rc<RefCell<Vec<u8>>>,
+    pub cartridge: Arc<Mutex<Vec<u8>>>,
 
-    pub memory: Rc<RefCell<InternalMemory>>,
+    pub memory: Arc<Mutex<InternalMemory>>,
 
-    pub lcd: Rc<RefCell<Box<GbaLcd>>>,
+    pub lcd: Arc<Mutex<Box<GbaLcd>>>,
     pub ppu: PixelProcessUnit,
 }
 
 impl Gba {
-    pub fn new(cartridge_header: CartridgeHeader, cartridge: Rc<RefCell<Vec<u8>>>) -> Self {
-        let lcd = Rc::new(RefCell::new(Box::new(GbaLcd::new())));
-        let memory = Rc::new(RefCell::new(InternalMemory::new()));
-        let ppu = PixelProcessUnit::new(lcd.clone(), memory.clone());
-        let arm = Arm7tdmi::new(cartridge.clone(), memory.clone());
+    pub fn new(cartridge_header: CartridgeHeader, cartridge: Arc<Mutex<Vec<u8>>>) -> Self {
+        let lcd = Arc::new(Mutex::new(Box::new(GbaLcd::new())));
+        let memory = Arc::new(Mutex::new(InternalMemory::new()));
+        let ppu = PixelProcessUnit::new(Arc::clone(&lcd), Arc::clone(&memory));
+        let arm = Arm7tdmi::new(Arc::clone(&cartridge), Arc::clone(&memory));
         Self {
             cpu: arm,
             cartridge,
