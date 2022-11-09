04b841e829b85fd86e19bde3ac87f29b6463f4db
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-11-09 21:16:37 +0100

    Logger: Add new logger for `clementine`
    This should enable a possible log on output or on file for analyzing the instruction post run.
    
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

---
+++ logger/src/lib.rs
@@ -0,0 +1,135 @@
+use std::{
+    fs::File,
+    io::{self, Write},
+    sync::Mutex,
+    time::Instant,
+};
+
+use chrono::Utc;
+use once_cell::sync::OnceCell;
+
+static LOGGER: OnceCell<Logger> = OnceCell::new();
+
+struct LoggerImpl {
+    pub sink: Box<dyn Write + Send>,
+    pub start_instant: Instant,
+}
+
+impl LoggerImpl {
+    fn new(kind: LogKind) -> Self {
+        let start_instant = Instant::now();
+        match kind {
+            LogKind::STDOUT => Self {
+                sink: Box::new(io::stdout()),
+                start_instant,
+            },
+            LogKind::FILE => {
+                let now = Utc::now();
+                let filename = format!("clementine-{}.log", now.timestamp());
+                let path = std::env::temp_dir().join(filename);
+                Self {
+                    sink: Box::new(File::create(path).unwrap()),
+                    start_instant,
+                }
+            }
+        }
+    }
+
+    fn log<T>(&mut self, data: T)
+    where
+        T: std::fmt::Display,
+    {
+        let now = self.start_instant.elapsed();
+        let seconds = now.as_secs();
+        let hours = seconds / 3600;
+        let minutes = (seconds / 60) % 60;
+        let seconds = seconds % 60;
+        let miliseconds = now.subsec_millis();
+
+        let _ = writeln!(
+            self.sink,
+            "[{:02}:{:02}:{:02}.{:03}] {}",
+            hours, minutes, seconds, miliseconds, data
+        );
+    }
+}
+
+/// LogKind represents the kind of logging: `stdout` or `logfile`.
+#[derive(PartialEq, Eq)]
+pub enum LogKind {
+    /// It logs to console, the default choice.
+    STDOUT,
+
+    /// It logs on a file in /tmp/clementine-<timestamp>.log
+    FILE,
+}
+
+/// Logger
+struct Logger {
+    pub inner_impl: Mutex<LoggerImpl>,
+}
+
+impl Default for Logger {
+    fn default() -> Self {
+        Self {
+            inner_impl: Mutex::new(LoggerImpl::new(LogKind::STDOUT)),
+        }
+    }
+}
+
+impl Logger {
+    fn new(kind: LogKind) -> Self {
+        Self {
+            inner_impl: Mutex::new(LoggerImpl::new(kind)),
+        }
+    }
+
+    fn log<T>(&self, data: T)
+    where
+        T: std::fmt::Display,
+    {
+        if let Ok(ref mut inner) = self.inner_impl.lock() {
+            inner.log(data);
+        }
+    }
+}
+
+pub fn init_logger(kind: LogKind) {
+    _ = LOGGER.set(Logger::new(kind));
+}
+
+pub fn log<T>(data: T)
+where
+    T: std::fmt::Display,
+{
+    LOGGER.get().map_or((), |logger| logger.log(data));
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use crate::{init_logger, log, LogKind};
+
+    #[test]
+    fn logger_file() {
+        init_logger(LogKind::FILE);
+        log("ok".to_string());
+        let dir = std::env::temp_dir();
+        let files = fs::read_dir(dir).unwrap();
+        for f in files {
+            if let Ok(ff) = f {
+                let p = ff.path();
+                if let Some(ext) = p.extension() {
+                    let s = p.to_str().unwrap();
+                    if ext == "log" && s.contains(&"clementine") {
+                        print!("{p:?}");
+                        let s = fs::read_to_string(p.clone()).unwrap();
+                        fs::remove_file(p).unwrap();
+                        assert_eq!(s, "[00:00:00.000] ok\n".to_string());
+                    }
+                }
+            }
+        }
+    }
+}
