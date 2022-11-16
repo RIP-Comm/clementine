700c23e31d976bb17131b0574817c1ec9b643fa5
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-11-16 22:17:54 +0100

    ARM: Add docs to condition module
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- emu/src/arm/condition.rs
+++ emu/src/arm/condition.rs
@@ -1,21 +1,58 @@
-/// Arm opcode condition.
+/// In ARM state, all instructions are conditionally executed according to the state of the CPSR,
+///  condition codes and the instruction’s condition field.
+/// This field (bits 31:28) determines the circumstances under which an instruction is to be executed.
+/// If the state of the C, N, Z and V flags fulfils the conditions encoded by the field,
+/// the instruction is executed, otherwise it is ignored.
+/// In the absence of a suffix, the condition field of most instructions is set to "Always" (sufix AL).
+/// This means the instruction will always be executed regardless of the CPSR condition codes.
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub enum Condition {
+    /// Z set (equal).
     EQ = 0x0,
+
+    /// Z clear (not equal).
     NE = 0x1,
+
+    /// C set (unsigned higher or same).
     CS = 0x2,
+
+    /// C clear (unsigned lower).
     CC = 0x3,
+
+    /// N set (negative).
     MI = 0x4,
+
+    /// N clear (positive or zero).
     PL = 0x5,
+
+    /// V set (overflow).
     VS = 0x6,
+
+    /// V clear (no overflow).
     VC = 0x7,
+
+    /// C set and Z clear (unsigned higher).
     HI = 0x8,
+
+    /// C clear or Z set (unsigned lower or same).
     LS = 0x9,
+
+    /// N equals V (greater or equal).
     GE = 0xA,
+
+    /// N not equal to V (less then).
     LT = 0xB,
+
+    /// Z clear AND (N equals V) (greater then).
     GT = 0xC,
+
+    /// Z set OR (N not equals V) (less then or equal).
     LE = 0xD,
+
+    /// ignored.
     AL = 0xE,
+
+    /// The sixteenth (1111) is reserved, and must not be used.
     NV = 0xF,
 }
 
