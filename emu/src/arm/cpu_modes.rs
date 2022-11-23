e4b848f2fe624cc8d4c33fb12fd4119e58516ca8
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-11-23 19:59:18 +0100

    Add doc for CPU modes
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- emu/src/arm/cpu_modes.rs
+++ emu/src/arm/cpu_modes.rs
@@ -1,11 +1,24 @@
 #[derive(Debug, PartialEq, Eq)]
 pub enum Mode {
+    /// The normal ARM program execution state.
     User = 0b10000,
+
+    /// Designed to support a data transfer or channel process.
     Fiq = 0b10001,
+
+    /// Used for general-purpose interrupt handling.
     Irq = 0b10010,
+
+    /// Protected mode for the operating system
     Supervisor = 0b10011,
+
+    /// Entered after a data or instruction prefetch abort.
     Abort = 0b10111,
+
+    /// Entered when an undefined instruction is executed
     Undefined = 0b11011,
+
+    /// A privileged user mode for the operating system.
     System = 0b11111,
 }
 
