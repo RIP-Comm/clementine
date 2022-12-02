7540b1a9c2ae03cc578a20918fb0d5d2c96128ea
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-12-02 23:16:03 +0100

    GBA: Remove ROM data from struct for UI
    Right now we don't use this for any debugger viewer, then I prefer to
    keep as simple as possible as stay with a plan vec.
    
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- emu/src/gba.rs
+++ emu/src/gba.rs
@@ -11,7 +11,6 @@ pub struct Gba {
     pub cpu: Arm7tdmi,
 
     pub cartridge_header: CartridgeHeader,
-    pub cartridge: Arc<Mutex<Vec<u8>>>,
 
     pub memory: Arc<Mutex<InternalMemory>>,
 
@@ -23,18 +22,14 @@ impl Gba {
     pub fn new(
         cartridge_header: CartridgeHeader,
         bios: [u8; 0x00004000],
-        cartridge: Arc<Mutex<Vec<u8>>>,
+        cartridge: Vec<u8>,
     ) -> Self {
         let lcd = Arc::new(Mutex::new(Box::new(GbaLcd::new())));
-        let memory = Arc::new(Mutex::new(InternalMemory::new(
-            bios,
-            Arc::clone(&cartridge),
-        )));
+        let memory = Arc::new(Mutex::new(InternalMemory::new(bios, cartridge)));
         let ppu = PixelProcessUnit::new(Arc::clone(&lcd), Arc::clone(&memory));
         let arm = Arm7tdmi::new(Arc::clone(&memory));
         Self {
             cpu: arm,
-            cartridge,
             cartridge_header,
             ppu,
             lcd,
