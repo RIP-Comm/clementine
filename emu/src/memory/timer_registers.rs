c947c8b0a0a94786b4d8889e08c9cc7355bdd213
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-12-03 13:14:30 +0100

    Memory: Refactor timer as IO device
    Now timer implement trait `IoDevice`, this allow bettere redability and
    maybe a future possible mock or implemetation :)
    
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- emu/src/memory/timer_registers.rs
+++ emu/src/memory/timer_registers.rs
@@ -1,4 +1,9 @@
-use crate::memory::io_registers::{IORegister, IORegisterAccessControl};
+use crate::{
+    bitwise::Bits,
+    memory::io_registers::{IORegister, IORegisterAccessControl},
+};
+
+use super::io_device::IoDevice;
 
 pub struct TimerRegisters {
     /// Timer 0 Counter/Reload
@@ -38,6 +43,41 @@ impl TimerRegisters {
             tm2cnt_h: IORegister::with_access_control(ReadWrite),
             tm3cnt_l: IORegister::with_access_control(ReadWrite),
             tm3cnt_h: IORegister::with_access_control(ReadWrite),
+        }
+    }
+}
+
+impl IoDevice for TimerRegisters {
+    type Address = usize;
+    type Value = u8;
+
+    // There is no need to set the second byte because bits `8-15` are not used
+    fn write_at(&mut self, address: usize, value: u8) {
+        match address {
+            0x04000100 => self.tm0cnt_l.set_byte(0, value),
+            0x04000102 => self.tm0cnt_h.set_byte(0, value),
+            0x04000104 => self.tm1cnt_l.set_byte(0, value),
+            0x04000106 => self.tm1cnt_h.set_byte(0, value),
+            0x04000108 => self.tm2cnt_l.set_byte(0, value),
+            0x0400010A => self.tm2cnt_h.set_byte(0, value),
+            0x0400010C => self.tm3cnt_l.set_byte(0, value),
+            0x0400010E => self.tm3cnt_h.set_byte(0, value),
+            _ => panic!("Reading an write-only memory address: {address:b}"),
+        }
+    }
+
+    // There is no need to read the second byte because bits `8-15` are not used
+    fn read_at(&self, address: usize) -> u8 {
+        match address {
+            0x04000100 => self.tm0cnt_l.read().get_byte(0),
+            0x04000102 => self.tm0cnt_h.read().get_byte(0),
+            0x04000104 => self.tm1cnt_l.read().get_byte(0),
+            0x04000106 => self.tm1cnt_h.read().get_byte(0),
+            0x04000108 => self.tm2cnt_l.read().get_byte(0),
+            0x0400010A => self.tm2cnt_h.read().get_byte(0),
+            0x0400010C => self.tm3cnt_l.read().get_byte(0),
+            0x0400010E => self.tm3cnt_h.read().get_byte(0),
+            _ => panic!("Reading an write-only memory address: {address:b}"),
         }
     }
 }
