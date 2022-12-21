bbf21d3867287dcb4c643f6aefddc9fdc0aa0cce
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-12-21 12:50:20 +0100

    UI: Add disassembler viewer
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

---
+++ ui/src/disassembler.rs
@@ -0,0 +1,53 @@
+use std::sync::{Arc, Mutex};
+
+use egui::{ScrollArea, TextEdit, TextStyle};
+use emu::gba::Gba;
+
+use crate::ui_traits::{UiTool, View};
+
+pub struct Disassembler {
+    gba: Arc<Mutex<Gba>>,
+}
+
+impl Disassembler {
+    pub(crate) fn new(arc_gba: Arc<Mutex<Gba>>) -> Self {
+        Self { gba: arc_gba }
+    }
+}
+
+impl UiTool for Disassembler {
+    fn name(&self) -> &'static str {
+        "Disassembler"
+    }
+
+    fn show(&mut self, ctx: &egui::Context, open: &mut bool) {
+        egui::Window::new(self.name())
+            .resizable(true)
+            .open(open)
+            .show(ctx, |ui| {
+                use View as _;
+                self.ui(ui);
+            });
+    }
+}
+
+impl View for Disassembler {
+    fn ui(&mut self, ui: &mut egui::Ui) {
+        let mut s = self.gba.lock().unwrap().cpu.current_instruction.join("\n");
+
+        ScrollArea::vertical().show(ui, |ui| {
+            ui.add(
+                TextEdit::multiline(&mut s)
+                    .interactive(false)
+                    .font(TextStyle::Monospace)
+                    .layouter(&mut |ui, val, _| {
+                        ui.fonts().layout_no_wrap(
+                            val.to_string(),
+                            TextStyle::Monospace.resolve(ui.style()),
+                            ui.visuals().widgets.inactive.text_color(),
+                        )
+                    }),
+            );
+        });
+    }
+}
