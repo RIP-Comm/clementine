1cd2214ac919b310fa9234c5d97a4a790446bec3
Federico Guerinoni <guerinoni.federico@gmail.com>
2023-01-16 20:07:12 +0100

    CPU: Add more unused interrupt
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- emu/src/memory/interrupts.rs
+++ emu/src/memory/interrupts.rs
@@ -20,6 +20,30 @@ pub struct Interrupts {
     /// 0x400020A       -    -         Not used
     not_used_a: IORegister,
 
+    /// 0x400020C       -    -         Not used
+    not_used_c: IORegister, // FIXME: Not sure about this
+
+    /// 0x400020E       -    -         Not used
+    not_used_e: IORegister, // FIXME: Not sure about this
+
+    /// 0x4000210       -    -         Not used
+    not_used_10: IORegister, // FIXME: Not sure about this
+
+    /// 0x4000212       -    -         Not used
+    not_used_12: IORegister, // FIXME: Not sure about this
+
+    /// 0x4000214       -    -         Not used
+    not_used_14: IORegister, // FIXME: Not sure about this
+
+    /// 0x4000216       -    -         Not used
+    not_used_16: IORegister, // FIXME: Not sure about this
+
+    /// 0x4000218       -    -         Not used
+    not_used_18: IORegister, // FIXME: Not sure about this
+
+    /// 0x400021A       -    -         Not used
+    not_used_1a: IORegister, // FIXME: Not sure about this
+
     /// Interrupt Master Enable Register
     ime: IORegister,
 
@@ -53,6 +77,14 @@ impl Interrupts {
             ime: IORegister::with_access_control(ReadWrite),
             not_used_06: IORegister::with_access_control(ReadWrite),
             not_used_a: IORegister::with_access_control(ReadWrite),
+            not_used_c: IORegister::with_access_control(ReadWrite),
+            not_used_e: IORegister::with_access_control(ReadWrite),
+            not_used_10: IORegister::with_access_control(ReadWrite),
+            not_used_12: IORegister::with_access_control(ReadWrite),
+            not_used_14: IORegister::with_access_control(ReadWrite),
+            not_used_16: IORegister::with_access_control(ReadWrite),
+            not_used_18: IORegister::with_access_control(ReadWrite),
+            not_used_1a: IORegister::with_access_control(ReadWrite),
         }
     }
 }
@@ -73,6 +105,22 @@ impl IoDevice for Interrupts {
             0x04000207 => self.not_used_06.read().get_byte(1),
             0x0400020A => self.not_used_a.read().get_byte(0),
             0x0400020B => self.not_used_a.read().get_byte(1),
+            0x0400020C => self.not_used_c.read().get_byte(0),
+            0x0400020D => self.not_used_c.read().get_byte(1),
+            0x0400020E => self.not_used_e.read().get_byte(0),
+            0x0400020F => self.not_used_e.read().get_byte(1),
+            0x04000210 => self.not_used_10.read().get_byte(0),
+            0x04000211 => self.not_used_10.read().get_byte(1),
+            0x04000212 => self.not_used_12.read().get_byte(0),
+            0x04000213 => self.not_used_12.read().get_byte(1),
+            0x04000214 => self.not_used_14.read().get_byte(0),
+            0x04000215 => self.not_used_14.read().get_byte(1),
+            0x04000216 => self.not_used_16.read().get_byte(0),
+            0x04000217 => self.not_used_16.read().get_byte(1),
+            0x04000218 => self.not_used_18.read().get_byte(0),
+            0x04000219 => self.not_used_18.read().get_byte(1),
+            0x0400021A => self.not_used_1a.read().get_byte(0),
+            0x0400021B => self.not_used_1a.read().get_byte(1),
             0x04000300 => self.post_flag.read().get_byte(0),
             0x04000208 => self.ime.read().get_byte(0),
             0x04000209 => self.ime.read().get_byte(1),
@@ -92,6 +140,22 @@ impl IoDevice for Interrupts {
             0x04000207 => self.not_used_06.set_byte(1, value),
             0x0400020A => self.not_used_a.set_byte(0, value),
             0x0400020B => self.not_used_a.set_byte(1, value),
+            0x0400020C => self.not_used_c.set_byte(0, value),
+            0x0400020D => self.not_used_c.set_byte(1, value),
+            0x0400020E => self.not_used_e.set_byte(0, value),
+            0x0400020F => self.not_used_e.set_byte(1, value),
+            0x04000210 => self.not_used_10.set_byte(0, value),
+            0x04000211 => self.not_used_10.set_byte(1, value),
+            0x04000212 => self.not_used_12.set_byte(0, value),
+            0x04000213 => self.not_used_12.set_byte(1, value),
+            0x04000214 => self.not_used_14.set_byte(0, value),
+            0x04000215 => self.not_used_14.set_byte(1, value),
+            0x04000216 => self.not_used_16.set_byte(0, value),
+            0x04000217 => self.not_used_16.set_byte(1, value),
+            0x04000218 => self.not_used_18.set_byte(0, value),
+            0x04000219 => self.not_used_18.set_byte(1, value),
+            0x0400021A => self.not_used_1a.set_byte(0, value),
+            0x0400021B => self.not_used_1a.set_byte(1, value),
             0x04000300 => self.post_flag.set_byte(0, value),
             0x04000208 => self.ime.set_byte(0, value),
             0x04000209 => self.ime.set_byte(1, value),
