3cbd3849a9e7b9786bb7aa6e0835a8302792a15d
Federico Guerinoni <guerinoni.federico@gmail.com>
2023-04-07 22:58:39 +0200

    Thumb: Add ASM for `HiRegisterOpBX`
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- emu/src/cpu/arm7tdmi.rs
+++ emu/src/cpu/arm7tdmi.rs
@@ -1872,11 +1872,24 @@ mod tests {
             let mut cpu = Arm7tdmi::default();
             let op_code: u16 = 0b0100_0111_0111_0000;
             let op_code: ThumbModeOpcode = cpu.decode(op_code);
+<<<<<<< HEAD
             assert_eq!(op_code.instruction, ThumbModeInstruction::HiRegisterOpBX{
                 op: 0b11,
                 reg_source: 14,
                 reg_destination: 0,
             });
+||||||| parent of 98f86d9 (Thumb: Add ASM for `HiRegisterOpBX`)
+            assert_eq!(op_code.instruction, ThumbModeInstruction::HiRegisterOpBX);
+=======
+            assert_eq!(
+                op_code.instruction,
+                ThumbModeInstruction::HiRegisterOpBX {
+                    op: 0b11,
+                    reg_source: 14,
+                    reg_destination: 0,
+                }
+            );
+>>>>>>> 98f86d9 (Thumb: Add ASM for `HiRegisterOpBX`)
 
             cpu.registers.set_register_at(14, 123);
             cpu.execute_thumb(op_code);
